c6bf3931603ef9880f860fa80895f3ffdcbfa61d
이유진 <youjin@iyujin-ui-MacBookAir.local>
2022-02-28 21:28:44 +0900

    Add : models.py 작성 및 settigs.py db option 추가

--- board/models.py
+++ board/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from users.models import User
+from core.models import TimeStampModel
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    main_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='Category')
+    
+    class Meta:
+        db_table = 'categories'
+
+class Post(TimeStampModel):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    read_count = models.ImageField()
+
+    class Meta:
+        db_table = 'posts'
+
+class Comment(TimeStampModel):
+    commnet = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    main_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True )
+    
+    class Meta:
+        db_table = 'comments'
+
+class TagPost(models.Model):
+    tag = models.ForeignKey('Tag', on_delete=models.RESTRICT)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'tag_posts'
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100)
+    posts = models.ManyToManyField('Post', through=TagPost)
+
+    class Meta:
+        db_table = 'tags'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.models import TimeStampModel
 
-# Create your models here.
+class User(TimeStampModel):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    profile_image_url = models.ImageField(max_length=200)
+    description = models.TextField()
+    kakao_id = models.IntegerField()
+    email = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = 'users'
+
+class Like(models.Model):
+    isLiked = models.BooleanField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    post = models.ForeignKey('board.Post', on_delete=models.CASCADE)
+    comment = models.ForeignKey('board.Comment', on_delete=models.CASCADE) 
+    
+    class Meta:
+        db_table = 'likes'
