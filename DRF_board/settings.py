7945525d7da3ebf5a9491e530f3d7a2533ab4cc8
이유진 <youjin@iyujin-ui-MacBookAir.local>
2022-03-02 15:16:44 +0900

    Modified : 1.  model 수정, 2.  users.view 작성ing~ Add 1. Kakao_login api 구현(django 기반)

--- DRF_board/settings.py
+++ DRF_board/settings.py
@@ -30,14 +30,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
-    #'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -48,14 +48,15 @@ INSTALLED_APPS = [
     'board',
     'core',
     'users',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
---
+++ users/serializers.py
@@ -0,0 +1,6 @@
+# from users.models import User
+
+# class UserSerializer(serializer.ModelsSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['name', 'phone_number', 'kakao_id', 'email']    
