c0701f864fddba1b8e651a2818ed59ab5d0fcea2
이유진 <youjin@iyujin-ui-MacBookAir.local>
2022-03-03 19:21:46 +0900

    Add : Kakao_login api 구현(django 기반)

--- users/views.py
+++ users/views.py
@@ -1,64 +1,49 @@
 import requests, jwt, os
-from rest_framework.response import Response
+
 from rest_framework import status
 from django.views import View
+from django.http import JsonResponse
 from users.models import User
 from dotenv import load_dotenv
 load_dotenv()
 
-'''
-ACCESS_TOKEN = 
-# 프론트단이 토큰 받아올때의 json 형태(이미 로그인한 상태의 토큰이 발급됨)
-request = {
-    "token_type":"bearer",
-    "access_token":"${ACCESS_TOKEN}",
-    "expires_in":43199,
-    "refresh_token":"${REFRESH_TOKEN}",
-    "refresh_token_expires_in":25184000,
-    "scope":"account_email profile"
-}
-'''
-
-# class KakaoUserAPI:
 def get_kakao_user(access_token):
     kakao_url = 'https://kapi.kakao.com/v2/user/me'
-    headers = {'Authorization' : f'Bearer {access_token}'}
-    response = requests.get(kakao_url, headers = headers)
+    header = {'Authorization' : f'Bearer {access_token}'}
 
-    if not response.status_code == 200:
-        return Response({'message' : 'ResponseError'}, status=status.HTTP_400_BAD_REQUEST)
-    return response.json()
+    response = requests.get(kakao_url, headers=header)
+    
+    if response.status_code == 200:
+        return response.json()
+
+    elif response.status_code == 401:
+        return JsonResponse({'message':'UnAutorized'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    else:
+        return JsonResponse({'message':'ResponseError'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# 파라미터 : 액세스 토큰을 헤더에 get, post로 담아서 넘기기
-# 파라미터 : target_id,target_id_type, 사용자 회원번호 함께 전달(어드민키경우)
 class KakaoLoginView(View):
-    def post(self, request):
+    def get(self, request):
         try:
-            '''
-            받은 토큰 정보로 db에 유저 정보를 입력할것
-
-            '''
             kakao_access_token = request.headers['Authorization']
             user_info = get_kakao_user(kakao_access_token)
 
-            name = user_info['kakao_account']['profile']['nickname']
-            phone_number = user_info['kakao_account']['phone_number']
-            profile_image_url = user_info['kakao_account']['profile']['profile_image_url']
-            kakao_id =  user_info['id']
+            kakao_id = user_info['id']
             email = user_info['kakao_account']['email']
-            
-            if not User.objects.filter(email == email).exists():
-                user, is_created = User.objects.get_or_create(
-                name = name,
-                phone_number = phone_number,
-                profile_image_url = profile_image_url,
-                kakao_id = kakao_id,
-                email = email
-            )
-            
-            token = jwt.encode({"id" : user.id}, os.environ['SECRET_KEY'], os.environ['ALGORITHM'])
-            return Response({"token" : token}, status = status.HTTP_200_OK)
+            name = user_info['kakao_account']['profile']['nickname']
 
-        except:
-            return 
+            user, is_created = User.objects.get_or_create(
+                kakao_id = kakao_id,
+                email = email,
+                name = name,   
+            )
+           
+            token = jwt.encode({"id" : user.id}, os.environ['SECRET_KEY'], os.environ['ALGORITHM'])
+
+            if is_created == False:
+                return JsonResponse({"MESSAGE" : "SUCCESS", "Authorization" : token}, status = status.HTTP_200_OK)
+            return JsonResponse({"MESSAGE" : "CREATED", "Authorization" : token}, status = status.HTTP_201_CREATED)
+        
+        except KeyError:
+            return JsonResponse({"MESSAGE" : "KeyError"}, status=status.HTTP_400_BAD_REQUEST)
